a24c8675192548a02b33412ccee911582e7f2d1c
tead1234 <tead1234@gmail.com>
2022-11-03 23:11:05 +0900

    blog project start2

--- main.py
+++ main.py
@@ -6,16 +6,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {"da": "hji"}
+@app.get('/blog')
+## 쿼리변수를 함수에 직접 떄려박음
+# 디폴트값 넣기 가능
+def index(limit= 10, published = True):
+    if published:
+
+        return {"da": f'{limit}hji'}
+    else:
+        return "ERROR"
 @app.get('/blog/{id}')
 def comments(id: int):
     return {'data': {
         id
-    }   }
+        }
+    }
 
-@app.get('/blog/unpublished'):
+@app.get('/blog/unpublished')
 def unpublished():
     return {
         'data' : 'all unpublished blogs'
